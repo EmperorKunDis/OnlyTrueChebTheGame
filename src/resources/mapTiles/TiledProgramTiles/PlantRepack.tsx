98877691d05f18d2e1a6bf1cb5d2ecbbd2c9802a
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-12-13 00:05:32 +0100

    MapaVTiledu
    Tak snad to puijde

---
+++ src/resources/mapTiles/TiledProgramTiles/PlantRepack.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="plant repack" tilewidth="32" tileheight="32" tilecount="169" columns="13">
+ <image source="plant repack.png" width="416" height="416"/>
+ <tile id="95">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.25093" y="10.3775">
+    <polygon points="0,0 0.170122,21.6906 15.2259,21.1802 14.8857,-0.255183"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.33599" y="0.255183">
+    <polygon points="-0.170122,13.6948 0.170122,31.7278 15.311,31.6427 15.311,13.6948"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.42105" y="-0.0850611">
+    <polygon points="0,0 0,6.54971 -3.23232,6.29452 -1.19086,9.10154 -4.08293,11.6534 -3.74269,12.9293 -0.76555,12.504 0.935673,10.9729 3.57257,12.6741 3.99787,16.0766 6.03934,15.9915 9.44179,11.8235 13.4397,14.6305 14.8857,14.0351 13.5247,11.3131 17.1823,11.9086 19.5641,10.4625 17.3525,6.80489 15.4811,4.84848 15.2259,0.170122"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.35885" y="11.8235">
+    <polygon points="0,0 5.18873,-1.27592 8.08081,3.40245 11.9086,-0.170122 17.3525,2.38171 16.672,-1.19086 22.6263,-1.70122 19.0537,-5.95428 18.2031,-11.4833 2.97714,-11.4833 2.89208,-5.44391 0,-5.35885"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Nepojmenovaná sada" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="55" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="56" wangid="2,0,2,2,2,2,2,0"/>
+   <wangtile tileid="57" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="58" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="59" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="60" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="68" wangid="0,2,2,2,2,0,0,0"/>
+   <wangtile tileid="69" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="70" wangid="0,0,0,0,2,2,2,2"/>
+   <wangtile tileid="71" wangid="1,1,1,1,1,1,0,0"/>
+   <wangtile tileid="72" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="73" wangid="1,0,0,1,1,1,1,1"/>
+   <wangtile tileid="81" wangid="2,2,2,2,2,0,0,2"/>
+   <wangtile tileid="82" wangid="2,2,2,2,2,0,2,2"/>
+   <wangtile tileid="83" wangid="2,0,0,0,0,0,2,2"/>
+   <wangtile tileid="84" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="85" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="86" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="95" wangid="2,0,0,2,2,2,0,0"/>
+   <wangtile tileid="96" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="97" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="98" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="99" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="107" wangid="0,2,2,0,0,0,0,0"/>
+   <wangtile tileid="108" wangid="2,2,2,0,0,0,2,2"/>
+   <wangtile tileid="109" wangid="0,0,0,0,0,0,2,2"/>
+   <wangtile tileid="110" wangid="0,1,1,0,0,0,0,0"/>
+   <wangtile tileid="111" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="112" wangid="0,0,0,0,0,0,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ src/resources/mapTiles/TiledProgramTiles/Teren.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Teren" tilewidth="32" tileheight="32" tilecount="483" columns="21">
+ <image source="terrain.png" width="672" height="736"/>
+ <wangsets>
+  <wangset name="Nepojmenovaná sada" type="mixed" tile="64">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="1" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="2" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="21" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="22" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="23" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="42" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="43" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="44" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="63" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="64" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="65" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="84" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="85" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="86" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
